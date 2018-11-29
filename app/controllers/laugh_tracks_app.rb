a0b09fe8273b451c0dc6f3b5b189de9ae9038d17
Justin Mauldin <justin.mauldin7@gmail.com>
2018-11-29 10:14:01 -0700

    Adds method to get averageruntime to work.

--- app/controllers/laugh_tracks_app.rb
+++ app/controllers/laugh_tracks_app.rb
@@ -2,6 +2,7 @@ class LaughTracksApp < Sinatra::Base
   get '/comedians' do
     @comedians = Comedian.all
     @average_age = Comedian.average_age
+    @average_runtime = Special.average_runtime
     erb :index
   end
 end
--- app/models/special.rb
+++ app/models/special.rb
@@ -1,3 +1,7 @@
 class Special < ActiveRecord::Base
   belongs_to :comedian
+
+  def self.average_runtime
+    average(:runtime).to_f.round(2)
+  end
 end
